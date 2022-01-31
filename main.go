d76b44c60280f5859759573905b1fb915974f8f0
Utkarsh Chaurasia <utkarsh103b@gmail.com>
2022-01-31 15:52:45 +0530

    Implemeted MksPipelineRun controller
    - Use Tekton APIs
    - Add handlers to convert the mksPipelineRun to Tekton pipelinerun when a new
      resorce is added.

--- main.go
+++ main.go
@@ -1,9 +1,58 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"time"
+
+	examplecomclientset "github.com/MiniTeks/mks-server/pkg/client/clientset/versioned"
+	informers "github.com/MiniTeks/mks-server/pkg/client/informers/externalversions"
+	con "github.com/MiniTeks/mks-server/pkg/controllers/mkspipelinerun"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/klog/v2"
+)
+
+var (
+	kuberconfig = flag.String("kubeconfig", "", "Path to a kubeconfig. Only required if out-of-cluster.")
+	master      = flag.String("master", "", "The address of the Kubernetes API server. Overrides any value in kubeconfig. Only required if out-of-cluster.")
 )
 
 func main() {
-	fmt.Println("Hello mks-server")
+	klog.InitFlags(nil)
+	flag.Parse()
+
+	cfg, err := clientcmd.BuildConfigFromFlags(*master, *kuberconfig)
+	if err != nil {
+		klog.Fatalf("Error building kubeconfig: %v", err)
+	}
+
+	exampleClient, err := examplecomclientset.NewForConfig(cfg)
+	if err != nil {
+		klog.Fatalf("Error building example clientset: %v", err)
+	}
+
+	kubeClient, err := kubernetes.NewForConfig(cfg)
+	if err != nil {
+		klog.Fatalf("Error building example clientset: %v", err)
+	}
+
+	ch := make(chan struct{})
+
+	/* creating new instance of NewSharedInformerFactory instead of Informer to reduce the load on apiserver
+	   in case on n GVRs
+	   if resources of only a particular namespace is required then NewFilteredSharedInformerFactory can be
+	   used
+	*/
+	// sync in memory cache with kubernetes cluster state in every 10 min
+	informers := informers.NewSharedInformerFactory(exampleClient, 10*time.Minute)
+	c := con.NewController(kubeClient, exampleClient, informers.Mkscontroller().V1alpha1().MksPipelineRuns())
+
+	// starting informers
+	informers.Start(ch)
+
+	// starting controller by calling run() and passing channel ch
+	c.Run(ch)
+	fmt.Println(informers)
+
 }
