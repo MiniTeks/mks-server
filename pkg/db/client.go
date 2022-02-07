17fbb0eb78ace94bf7e6c5337e65ac1a2b1aa014
ramessesii2 <stmbhardwaj@gmail.com>
2022-02-07 14:42:09 +0530

    make datapoints keys stored as CAPITAL letters only in db
    Signed-off-by: Satyam Bhardwaj <stmbhardwaj@gmail.com>

--- pkg/db/client.go
+++ pkg/db/client.go
@@ -1,11 +1,14 @@
 package db
 
 import (
+	"context"
 	"fmt"
 	"log"
 
-	"github.com/go-redis/redis"
+	"github.com/go-redis/redis/v8"
 )
+
+var ctx = context.Background()
 
 func GetRedisClient(cred *RClient) *redis.Client {
 	rdb := redis.NewClient(&redis.Options{
@@ -13,11 +16,11 @@ func GetRedisClient(cred *RClient) *redis.Client {
 		Password: cred.Pass, // no password set
 		DB:       cred.Db,   // use default DB 0
 	})
-	keys := []string{"mksTaskcreated", "mksTaskdeleted", "mksTaskactive", "mksTaskfailed", "mksTaskRuncreated", "mksTaskRundeleted", "mksTaskRunactive", "mksTaskRunfailed", "mksPipelineRuncreated", "mksPipelineRundeleted", "mksPipelineRunactive", "mksPipelineRunfailed"}
-	for _, item := range keys {
-		rdb.Set(item, 0, 0)
-	}
-	ping, err := rdb.Ping().Result()
+	// keys := []string{"mksTaskcreated", "mksTaskdeleted", "mksTaskactive", "mksTaskfailed", "mksTaskRuncreated", "mksTaskRundeleted", "mksTaskRunactive", "mksTaskRunfailed", "mksPipelineRuncreated", "mksPipelineRundeleted", "mksPipelineRunactive", "mksPipelineRunfailed"}
+	// for _, item := range keys {
+	// 	rdb.Set(item, 0, 0)
+	// }
+	ping, err := rdb.Ping(ctx).Result()
 	if err != nil {
 		log.Fatal("Couldn't connect to redis-database")
 	}
